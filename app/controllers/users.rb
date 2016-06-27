979ac608aa53e1f16c76c8af7b7930ab065686a4
Ben Flores <benjaminhyw@gmail.com>
2016-06-27 13:51:53 -0700

    added a post "/users" route which create a new user

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,8 @@
 get '/users/new' do
   erb :'users/register'
 end
+
+post '/users' do
+  @user = User.creat(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+  redirect '/'
+end
