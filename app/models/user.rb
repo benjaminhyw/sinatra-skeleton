6003e5763d6cf6dc825681233ed8c594fa69594a
Ben Flores <benjaminhyw@gmail.com>
2016-06-27 14:25:06 -0700

    Generated basic User model, added methods, validations, and included BCrypt.. which means I have to require it where appropriate

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Remember to create a migration!
+  include BCrypt
+  
+  validates_presence_of :email, :password
+  validates :email, uniqueness: true
+  validates :hashed_password, uniqueness: true
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user.password == password
+      return @user
+    else
+      nil
+    end
+  end
+
+end
